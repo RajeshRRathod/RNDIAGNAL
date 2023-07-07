ab7f6cb653e5e85b85caaea5d4aedf1404292157
rajesh rathod <rajesh.rathod@solutionanalysts.com>
2023-07-07 18:31:52 +0530

    realease build

--- src/componet/MovieItem/MovieItem.js
+++ src/componet/MovieItem/MovieItem.js
@@ -1,32 +1,37 @@
-import React from 'react';
-import { Image, Text, View } from 'react-native';
-import FastImage from 'react-native-fast-image'
-
+import React, { useState } from 'react';
+import { Text, View, TouchableHighlight } from 'react-native';
+import FastImage from 'react-native-fast-image';
+import Tooltip from 'react-native-walkthrough-tooltip';
 import styles from './styles';
-
 import missingImage from '../../assets/images/placeholder_for_missing_posters.png';
+import { ALL_COLORS } from '../../utility/consts';
 
 const MovieItem = (props) => {
+    const [toolTipVisible, setToolTipVisible] = useState(false)
     return (
         <View style={styles.itemContainer}>
-            {/* <Image
-                resizeMode='cover'
-                source={props.url}
-                style={styles.moviePoster}
-            /> */}
             <FastImage
                 defaultSource={missingImage}
                 style={styles.moviePoster}
                 source={props.url}
                 resizeMode={FastImage.resizeMode.cover}
             />
-            <Text
-                style={styles.movie_name}
-                numberOfLines={1}
-                ellipsizeMode='tail'>
-                {props.data.name}
-            </Text>
-        </View>
+            <Tooltip
+                isVisible={toolTipVisible}
+                content={<Text style={{ color: ALL_COLORS.black }}>{props.data.name}</Text>}
+                placement="top"
+                onClose={() => setToolTipVisible(false)}
+            >
+                <TouchableHighlight onPress={() => setToolTipVisible(true)}>
+                    <Text
+                        style={styles.movie_name}
+                        numberOfLines={1}
+                        ellipsizeMode='tail'>
+                        {props.data.name}
+                    </Text>
+                </TouchableHighlight>
+            </Tooltip>
+        </View >
     );
 }
 
